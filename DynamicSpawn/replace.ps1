1ce0bf2cb03f7a510d7b5540794d5313f4b24d39
zekrom-vale <sgraven66@gmail.com>
2018-03-29 18:13:16 -0500

    Added a dynamic spawning tool

--- DynamicSpawn/replace.ps1
+++ DynamicSpawn/replace.ps1
@@ -1,8 +1,18 @@
-$val= Read-Host 'Value'
+$prev= Get-Content prevVal.txt
+$val= Read-Host 'New Value'
 $item = Get-ChildItem . *.json.patch -rec
-foreach ($file in $item)
-{
+foreach ($file in $item){
     (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace '"value": "human,apex,avian,hylotl,floran,glitch,kazdra"', $val } |
+    Foreach-Object { $_ -replace $prev, $val } |
     Set-Content $file.PSPath
 }
+$val| Set-Content 'prevVal.txt'
+
+$prev= Get-Content prevVal2.txt
+$val= Read-Host 'Second New Value'
+foreach ($file in $item){
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace $prev, $val } |
+    Set-Content $file.PSPath
+}
+$val| Set-Content 'prevVal2.txt'
