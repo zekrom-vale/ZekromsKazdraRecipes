704c68a2df63fd5003ead97392015ff127b14307
zekrom-vale <sgraven66@gmail.com>
2018-04-14 16:06:38 -0500

    Fixed overflow

--- ZekromsKazdraRecipes/ZekromsKazdraRecipes/scripts/multicraft.lua
+++ ZekromsKazdraRecipes/ZekromsKazdraRecipes/scripts/multicraft.lua
@@ -1,9 +1,9 @@
 function init()
+	--self.clock=0
 	self.recipes=root.assetJson(config.getParameter("recipefile"))
 	for key,value in pairs(self.recipes) do
 		if value["input"]==nil or value["output"]==nil then
-			sb.logWarn("Invalid recipe")
-			sb.logInfo(key)
+			sb.logWarn(sb.printJson(value,1))
 			table.remove(self.recipes, key)
 			key=key-1
 		end
@@ -14,14 +14,12 @@ function init()
 end
 
 function update(dt)
-	sb.logInfo("updateFN")
+	--self.clock=self.clock+1
 	storage.overflow=containerTryAdd(storage.overflow)
 	storage.overflow=containerTryAdd(storage.overflow)
-	sb.logInfo("update|overflowDN")
-	if storage.overflow==nil then
+	if type(storage.overflow)~="table" then
 		local stack=world.containerItems(entity.id())
 		for key,value in pairs(self.recipes) do
-			sb.logInfo("update|consumeItems")
 			storage.overflow=consumeItemsO(value["input"], value["output"], stack)
 			if storage.overflow~=false then	break	end
 		end
@@ -29,12 +27,10 @@ function update(dt)
 end
 
 function containerTryAdd(items)
-	if items==nil then	return nil	end
-	local ran,value=pcall(function(items) return containerAddItems(items) end)
-	if ran then
-		return value
-	else
+	if type(items)~="table" or next(items)==nil then
 		return nil
+	else
+		return containerAddItems(items)
 	end
 end
 
@@ -45,24 +41,21 @@ function containerAddItems(items)
 		local t=world.containerAddItems(id, item)
 		if type(t)=="table" then table.insert(arr, t) end
 	end
+	sb.logInfo(sb.printJson(arr,1))
 	return arr
 end
 
 function consumeItemsO(items, prod, stack) --In order
-	sb.logInfo("consumeItemsO")
 	for key,value in pairs(items) do
-		sb.logInfo("consumeItemsO|Has")
 		if stack[key]==nil then	return false	end
 		if not(value["name"]==stack[key]["name"] and value["count"]<stack[key]["count"]) then
 			return false
 		end
 	end
 	for key,value in pairs(items) do
-		sb.logInfo("consumeItemsO|consume")
 		world.containerConsume(entity.id(), value)
 	end
 	for key,value in pairs(prod) do
-		sb.logInfo("consumeItemsO|pool")
 		if value["pool"]~=nil then
 			local pool=root.createTreasure(value["pool"], value["level"] or 0)
 			table.remove(prod, key)
@@ -72,7 +65,6 @@ function consumeItemsO(items, prod, stack) --In order
 			end
 		end
 	end
-	sb.logInfo("consumeItems|ret")
 	return containerAddItems(prod)
 end
 
