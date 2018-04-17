5ba2032f6880f19042f7fb4a4a54d8d8c567af60
zekrom-vale <sgraven66@gmail.com>
2018-04-17 08:00:30 -0500

    Split lua files

---
+++ ZekromsKazdraRecipes/ZekromsKazdraRecipes/scripts/ZekromsContainerUtil.lua
@@ -0,0 +1,63 @@
+require "/scripts/ZekromsUtil.lua"
+Zcontainer={}
+
+function Zcontainer.tryAdd(items)
+	if type(items)~="table" or next(items)==nil then
+		return nil
+	else
+		return Zcontainer.addItems(items)
+	end
+end
+
+function Zcontainer.addItems(items)
+	local id=entity.id()
+	local arr={}
+	for _,item in pairs(items) do
+		local t=Zcontainer.putAt(item, self.output)
+		if type(t)=="table" then table.insert(arr, t) end
+	end
+	return arr
+end
+
+function Zcontainer.treasure(pass)
+	local items=Zutil.deepcopy(pass)
+	for key,item in pairs(items) do
+		if item.pool~=nil then
+			local pool=root.createTreasure(item.pool, item.level or 0)
+			table.remove(items, key)
+			key=key-1
+			for _,val in pairs(pool) do
+				table.insert(items, val)
+			end
+		end
+	end
+	return items
+end
+
+function Zcontainer.consumeAt(item, range)
+	local stack=world.containerItems(entity.id())
+	for offset=range[1],range[2] do
+		if stack[offset]~=nil and stack[offset]["name"]==item.name then
+			if stack[offset]["count"]>=item.count then
+				world.containerConsumeAt(entity.id(), offset-1, item.count)
+				return true
+			end
+			item.count=item.count-stack[offset]["count"]
+			world.containerTakeAt(entity.id(), offset)
+		end
+	end
+	return false
+end
+
+function Zcontainer.putAt(item, range)
+	local stack=world.containerItems(entity.id())
+	for offset=range[1],range[2] do
+		if stack[offset]==nil or stack[offset]["name"]==item.name then
+			item=world.containerPutItemsAt(entity.id(), item, offset-1)
+			if item==nil or next(item)==nil or item.count<=0 then
+				return true
+			end
+		end
+	end
+	return item
+end
--- ZekromsKazdraRecipes/ZekromsKazdraRecipes/scripts/multicraft.lua
+++ ZekromsKazdraRecipes/ZekromsKazdraRecipes/scripts/multicraft.lua
@@ -1,3 +1,5 @@
+require "/scripts/ZekromsContainerUtil.lua"
+require "/scripts/ZekromsUtil.lua"
 function init()
 	if storage.clock==nil then
 		storage.clock=0
@@ -11,8 +13,8 @@ end
 
 function verify()
 	if #self.input>2 or #self.output>2 then
-		logModAPI()
-		sb.logInfo("Input/output array is not 2 elements for: "..sbName()..".  Ignoring the other ones")
+		Zutil.API()
+		sb.logInfo("Input/output array is not 2 elements for: "..Zutil.sbName()..".  Ignoring the other ones")
 	end
 	local size=world.containerSize(entity.id())
 	self.input={fixIO(self.input[1],size) or 1, fixIO(self.input[2],size) or size}
@@ -23,28 +25,28 @@ function verify()
 		self.input[1]=self.input[2]
 		self.input[2]=t
 		t=nil
-		logModAPI()
-		sb.logInfo("Input values swapped, please use [small, large] for: "..sbName())
+		Zutil.API()
+		sb.logInfo("Input values swapped, please use [small, large] for: "..Zutil.sbName())
 	end
 	if self.output[1]>self.output[2] then
 		local t=self.output[2]
 		self.output[1]=self.output[2]
 		self.output[2]=t
 		t=nil
-		logModAPI()
-		sb.logInfo("Output values swapped, please use [small, large] for: "..sbName())
+		Zutil.API()
+		sb.logInfo("Output values swapped, please use [small, large] for: "..Zutil.sbName())
 	end
 	verifyIn()
 	if next(self.recipes)==nil then
-		logModAPI()
-		sb.logError("No recipe file defined for: "..sbName())
+		Zutil.API()
+		sb.logError("No recipe file defined for: "..Zutil.sbName())
 	end
 end
 
 function fixIO(item,size)
 	if item==nil or type(item)~="number" or item%1~=0 or item>size or item<=0 then
-		logModAPI()
-		sb.logWarn("Input/output array is not 2 elements for: "..sbName()..".  Trying to compensate")
+		Zutil.API()
+		sb.logWarn("Input/output array is not 2 elements for: "..Zutil.sbName()..".  Trying to compensate")
 		return nil
 	end
 	return item
@@ -58,27 +60,27 @@ function verifyIn()
 	end
 	for key,value in pairs(self.recipes) do
 		if value.input==nil or value.output==nil then
-			logModAPI(); sb.logWarn("Input/output missing for: "..sbName())
+			Zutil.API(); sb.logWarn("Input/output missing for: "..Zutil.sbName())
 			key=act(value,key)
 			goto testEnd
 		elseif #value.input>self.input[2]-self.input[1]+1 then
-			logModAPI(); sb.logWarn("Input overflow in: "..sbName())
+			Zutil.API(); sb.logWarn("Input overflow in: "..Zutil.sbName())
 			key=act(value,key)
 			goto testEnd
 		elseif #value.output>self.output[2]-self.output[1]+1 then
-			logModAPI(); sb.logInfo("Output overflow in: "..sbName())
+			Zutil.API(); sb.logInfo("Output overflow in: "..Zutil.sbName())
 			sb.logInfo(sb.printJson(value,1))
 		end
 		if value.delay~=nil and value.delay%1~=0 then
-			logModAPI(); sb.logInfo("Dellay is not an 'int' in: "..sbName())
+			Zutil.API(); sb.logInfo("Dellay is not an 'int' in: "..Zutil.sbName())
 			sb.logInfo(sb.printJson(value,1))
 			value.delay=math.floor(value.delay)
 		end
 		for _,out in pairs(value.output) do
 			if out.pool==nil then	break	end
 			if not root.isTreasurePool(out.pool) then
-				logModAPI()
-				sb.logWarn("Invalid pool in: "..sbName())
+				Zutil.API()
+				sb.logWarn("Invalid pool in: "..Zutil.sbName())
 				key=act(value,key)
 				goto testEnd
 			end
@@ -87,27 +89,16 @@ function verifyIn()
 	end
 end
 
-
-function logModAPI()
-	sb.logInfo("---  API mod: 'ZekromsMulticraftAPI'  ---")
-end
-
-function sbName()
-	local name=config.getParameter("objectName")
-	if name~=nil then	name=config.getParameter("itemName")	end
-	return "'"..(name or "null").."'"
-end
-
 function update(dt)
 	storage.clock=(storage.clock+1)%self.clockMax
 	if storage.wait~=nil and storage.wait==storage.clock then
-		storage.overflow=containerTryAdd(storage.overflow)
+		storage.overflow=Zcontainer.tryAdd(storage.overflow)
 		storage.wait=nil
 		return
 	elseif storage.wait~=nil then
 		return
 	end
-	storage.overflow=containerTryAdd(storage.overflow)
+	storage.overflow=Zcontainer.tryAdd(storage.overflow)
 	if type(storage.overflow)~="table" then
 		local stack=world.containerItems(entity.id())
 		for _,value in pairs(self.recipes) do
@@ -122,24 +113,6 @@ function update(dt)
 	end
 end
 
-function containerTryAdd(items)
-	if type(items)~="table" or next(items)==nil then
-		return nil
-	else
-		return containerAddItems(items)
-	end
-end
-
-function containerAddItems(items)
-	local id=entity.id()
-	local arr={}
-	for _,item in pairs(items) do
-		local t=containerPutAt(item, self.output)
-		if type(t)=="table" then table.insert(arr, t) end
-	end
-	return arr
-end
-
 function consumeItemsShaped(items, prod, stacks, delay) --In order
 	for key,item in pairs(items) do
 		local stack=stacks[key+self.input[1]-1]
@@ -149,14 +122,14 @@ function consumeItemsShaped(items, prod, stacks, delay) --In order
 		end
 	end
 	for _,item in pairs(items) do
-		containerConsumeAt(item, self.input)
+		Zcontainer.consumeAt(item, self.input)
 	end
-	prod=treasure(prod)
+	prod=Zcontainer.treasure(prod)
 	if not(delay==nil or delay==0) then
 		storage.wait=(storage.clock+delay)%self.clockMax
 		return prod
 	end
-	return containerAddItems(prod)
+	return Zcontainer.addItems(prod)
 end
 
 function consumeItems(items, prod, stack, delay) --No order
@@ -174,29 +147,14 @@ function consumeItems(items, prod, stack, delay) --No order
 		::skip::
 	end
 	for _,item in pairs(items) do
-		containerConsumeAt(item, self.input)
+		Zcontainer.consumeAt(item, self.input)
 	end
-	prod=treasure(prod)
+	prod=Zcontainer.treasure(prod)
 	if not(delay==nil or delay==0) then
 		storage.wait=(storage.clock+delay)%self.clockMax
 		return prod
 	end
-	return containerAddItems(prod)
-end
-
-function treasure(pass)
-	local items=deepcopy(pass)
-	for key,item in pairs(items) do
-		if item.pool~=nil then
-			local pool=root.createTreasure(item.pool, item.level or 0)
-			table.remove(items, key)
-			key=key-1
-			for _,val in pairs(pool) do
-				table.insert(items, val)
-			end
-		end
-	end
-	return items
+	return Zcontainer.addItems(prod)
 end
 
 function die()
@@ -207,47 +165,4 @@ function die()
 			world.spawnItem(item.name, poz, item.count)
 		end
 	end
-end
-
-function containerConsumeAt(item, range)
-	local stack=world.containerItems(entity.id())
-	for offset=range[1],range[2] do
-		if stack[offset]~=nil and stack[offset]["name"]==item.name then
-			if stack[offset]["count"]>=item.count then
-				world.containerConsumeAt(entity.id(), offset-1, item.count)
-				return true
-			end
-			item.count=item.count-stack[offset]["count"]
-			world.containerTakeAt(entity.id(), offset)
-		end
-	end
-	return false
-end
-
-function containerPutAt(item, range)
-	local stack=world.containerItems(entity.id())
-	for offset=range[1],range[2] do
-		if stack[offset]==nil or stack[offset]["name"]==item.name then
-			item=world.containerPutItemsAt(entity.id(), item, offset-1)
-			if item==nil or next(item)==nil or item.count<=0 then
-				return true
-			end
-		end
-	end
-	return item
-end
-
---http://lua-users.org/wiki/CopyTable
-function deepcopy(orig)
-	local copy
-	if type(orig)=='table' then
-		copy={}
-		for orig_key,orig_value in next, orig, nil do
-			copy[deepcopy(orig_key)]=deepcopy(orig_value)
-		end
-		setmetatable(copy, deepcopy(getmetatable(orig)))
-	else
-		return orig
-	end
-	return copy
 end
